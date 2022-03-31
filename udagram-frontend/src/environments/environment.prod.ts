bfd6a4100b61ed3c8e9fe269329f5fc67939bbfe
Ali <a.sheriffouad@gmail.com>
2022-03-31 08:20:12 +0200

    fix db problem

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost:
-    'http://udagramdev-env.eba-2g6rpdbx.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-dev22222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
